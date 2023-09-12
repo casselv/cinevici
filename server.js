402e6d1dc3e1de5bc4642428278cd76a4f933c55
Vinnycassel <teddy0clark@gmail.com>
2023-09-12 22:43:08 +1000

    s

--- server.js
+++ server.js
@@ -84,7 +84,7 @@ app.post(
   }
 );
 
-const port = process.env.PORT || 3017;
+const port = process.env.PORT || 3018;
 
 app.listen(port, "0.0.0.0", function () {
   console.log(`Server is listening on port ${port}`);
