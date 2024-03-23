ce7e72899d29f7c3c939d5ff77f9f1b1c2d24cc6
Vinnycassel <teddy0clark@gmail.com>
2024-03-23 11:51:29 +1100

    aa

--- server.js
+++ server.js
@@ -23,10 +23,10 @@ app.get("/geosurf", (req, res) => {
 app.get("/propertylink", (req, res) => {
   res.render("propertylink");
 });
-/*
+
 app.get("/anantolink", (req, res) => {
   res.render("anatolink");
-});*/
+});
 
 app.post(
   "/submit-form",
@@ -101,7 +101,7 @@ app.post(
   }
 );
 
-const port = process.env.PORT || 3026;
+const port = process.env.PORT || 3027;
 
 app.listen(port, "0.0.0.0", function () {
   console.log(`Server is listening on port ${port}`);
